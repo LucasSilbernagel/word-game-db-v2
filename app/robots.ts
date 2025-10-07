02cf43aab396675c99c43827b1037f28171d0660
Lucas Silbernagel <lucas.silbernagel@gmail.com>
2025-10-06 22:41:19 -0400

    added sitemap

---
+++ app/robots.ts
@@ -0,0 +1,13 @@
+import { APP_METADATA } from '@/lib/constants'
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: ['/api/'],
+    },
+    sitemap: `${APP_METADATA.BASE_URL}/sitemap.xml`,
+  }
+}
---
+++ app/sitemap.ts
@@ -0,0 +1,28 @@
+import { APP_METADATA } from '@/lib/constants'
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = APP_METADATA.BASE_URL
+  const lastModified = new Date()
+
+  return [
+    {
+      url: baseUrl,
+      lastModified,
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+    {
+      url: `${baseUrl}/about`,
+      lastModified,
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/contact`,
+      lastModified,
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+  ]
+}
