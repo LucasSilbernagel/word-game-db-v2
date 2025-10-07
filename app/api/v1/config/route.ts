643b8f6286ff01d719a0a2e50aeb9a3635f6ce46
Lucas Silbernagel <lucas.silbernagel@gmail.com>
2025-10-06 22:09:22 -0400

    revert cors attempt

--- app/api/v1/config/route.ts
+++ app/api/v1/config/route.ts
@@ -1,11 +1,8 @@
-import { addCorsHeaders } from '@/lib/middleware'
 import { NextResponse } from 'next/server'
 
 export const GET = async () => {
-  const response = NextResponse.json({
+  return NextResponse.json({
     destructiveEndpointsEnabled:
       process.env.ENABLE_DESTRUCTIVE_ENDPOINTS === 'true',
   })
-
-  return addCorsHeaders(response)
 }
